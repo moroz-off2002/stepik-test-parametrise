3b3a8c27908f264b86cc4fc6bda2b31242829536
Alexander Morozov <122159176+moroz-off2002@users.noreply.github.com>
2023-07-21 13:11:25 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -2,9 +2,13 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-link = "http://selenium1py.pythonanywhere.com/"
+@pytest.mark.parametrize('language', ["ru", "en-gb"])
+class TestLogin:
+    def test_guest_should_see_login_link(self, browser, language):
+        link = f"http://selenium1py.pythonanywhere.com/{language}/"
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, "#login_link")
+        # этот тест запустится 2 раза
 
-
-def test_guest_should_see_login_link(browser):
-    browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, "#login_link")
+    def test_guest_should_see_navbar_element(self, browser, language):
+        # этот тест тоже запустится дважды
